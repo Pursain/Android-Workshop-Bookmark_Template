6aa5ec8e56a5577b1706540ee82bc1a95c9c4695
Harry <huanghg@uci.edu>
2019-02-16 00:51:47 -0800

    minor edits

--- app/src/main/java/com/example/harry/bookmark_template/SplashScreenActivity.java
+++ app/src/main/java/com/example/harry/bookmark_template/SplashScreenActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 public class SplashScreenActivity extends AppCompatActivity {
     //storing the views in variables is typically a good idea if you are going to use them a lot
     private TextView textView_splashAnswer;
-    private String currentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
